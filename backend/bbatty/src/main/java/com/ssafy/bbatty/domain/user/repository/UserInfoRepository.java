26e3f18ee65e3770fcdb999e65cc8158ec480b75
FlyBirds99 <shb9808@naver.com>
2025-07-25 12:35:32 +0900

    🚧 [BE-S13P11A403-131] AuthService 개발 중

--- backend/bbatty/src/main/java/com/ssafy/bbatty/domain/user/repository/UserInfoRepository.java
+++ backend/bbatty/src/main/java/com/ssafy/bbatty/domain/user/repository/UserInfoRepository.java
@@ -1,4 +1,19 @@
 package com.ssafy.bbatty.domain.user.repository;
 
-public interface UserInfoRepository {
+import com.ssafy.bbatty.domain.user.entity.User;
+import com.ssafy.bbatty.domain.user.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<User,Long> {
+
+    // 1. 카카오 로그인 검증 (POST /api/auth/kakao)
+    @Query("SELECT ui FROM UserInfo ui JOIN FETCH ui.user u JOIN FETCH u.team WHERE ui.kakaoId = :kakaoId")
+    Optional<UserInfo> findByKakaoIdWithUserAndTeam(@Param("kakaoId") String kakaoId);
+
+    // 2. 카카오 ID 존재 여부 확인 (회원가입 시 중복 체크)
+    boolean existsByKakaoId(String kakaoId);
 }
