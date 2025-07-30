c4e81476ac3c08f2ea87735a5af95d1660744904
DESKTOP-RBNO6LS\SSAFY <jamieh03@naver.com>
2025-07-30 17:23:15 +0900

    feat: image.tsx, button.tsx 수정

--- frontend/bbatty/src/shared/ui/image/image.tsx
+++ frontend/bbatty/src/shared/ui/image/image.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image as RNImage, ImageProps as RNImageProps } from 'react-native';
 
+// 추후 확장 시 필요
+// export interface ImageProps extends RNImageProps {}
+
 export const Image: React.FC<RNImageProps> = (props) => {
   return <RNImage {...props} />;
 };
